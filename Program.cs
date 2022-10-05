31af81f774522b8219ad7b734f6e08d8e42cdfb2
u21724 <cristiavaet@gmail.com>
2022-10-05 15:18:54 -0300

    [+] Sistema de Login

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using ProjetoEscola_API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
 //Criando o CORS
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
@@ -9,8 +13,10 @@ var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 //Fazendo o builder do CORS
-builder.Services.AddCors(options => {
-    options.AddPolicy(MyAllowSpecificOrigins, builder => { 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(MyAllowSpecificOrigins, builder =>
+    {
         builder.WithOrigins("http://localhost").AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
         builder.SetIsOriginAllowed(origin => new Uri(origin).Host == "localhost");
         builder.SetIsOriginAllowed(origin => true);
@@ -22,7 +28,29 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<EscolaContext>(options =>
 {
-options.UseSqlServer(builder.Configuration.GetConnectionString("StringConexaoSQLServer"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StringConexaoSQLServer"));
+});
+
+//JWT 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new
+    SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+    };
 });
 
 var app = builder.Build();
@@ -40,6 +68,10 @@ if (app.Environment.IsDevelopment())
 app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
+
+app.UseAuthorization();
+app.UseAuthorization();
+
 
 app.MapControllers();
 
